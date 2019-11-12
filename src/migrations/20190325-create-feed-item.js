518ec04596cb89ce6628749d91cf4e4ceb98b205
Benjie Velarde <benjamin.velarde@disney.com>
2019-11-12 17:33:39 +0800

    first commit

---
+++ src/migrations/20190325-create-feed-item.js
@@ -0,0 +1,28 @@
+'use strict';
+export function up(queryInterface, Sequelize) {
+  return queryInterface.createTable('FeedItem', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    caption: {
+      type: Sequelize.STRING
+    },
+    url: {
+      type: Sequelize.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  });
+}
+export function down(queryInterface, Sequelize) {
+  return queryInterface.dropTable('FeedItem');
+}
